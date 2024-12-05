67fa4c205877c5e0c07d0811d2f0474f6371538a
zahid-official <zahid.official8@gmail.com>
2024-12-05 09:17:55 +0600

    Home Page Path Change

--- client/src/Routes/Router.jsx
+++ client/src/Routes/Router.jsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
-import Home from "../Pages/Home";
+import Home from "../Components/Home/Home";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
     children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        }
-    ]
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
